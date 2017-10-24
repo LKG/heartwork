b7ff2172979aa4ea4fdcb20289708878f7f4f14e
gg <lkg61230413@163.com>
2017-10-24 15:27:29 +0800

    初始化导入

---
+++ heartwork/work-usercore/src/main/java/im/heart/usercore/exception/IncorrectCredentialsException.java
@@ -0,0 +1,14 @@
+package im.heart.usercore.exception;
+
+public class IncorrectCredentialsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6819474331721989064L;
+
+	public IncorrectCredentialsException(String message) {
+	    super(message);
+	}
+
+}
