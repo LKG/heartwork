6a4b8689cccc143e5b8cb9fb7b642831d2c52b0c
gg <lkg61230413@163.com>
2017-10-25 09:48:09 +0800

    操作日志工具类

---
+++ heartwork/work-usercore/src/main/java/im/heart/common/utils/OptLogUtils.java
@@ -0,0 +1,36 @@
+package im.heart.common.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import im.heart.common.context.ContextManager;
+import im.heart.core.log.OptLog;
+import im.heart.core.utils.BaseUtils;
+import im.heart.core.web.utils.WebUtilsEx;
+import im.heart.log.entity.FrameLogOperate;
+import im.heart.log.service.FrameLogOperateService;
+import im.heart.security.utils.SecurityUtilsHelper;
+
+public class OptLogUtils{
+	
+	public static  void optLog(HttpServletRequest request,OptLog optLog ){
+		FrameLogOperateService logLoginService=(FrameLogOperateService)ContextManager.getBean(FrameLogOperateService.BEAN_NAME);
+		String requestheaders = WebUtilsEx.getHeadersJson(request);
+		String accept = request.getHeader("accept");
+		String userAgent = request.getHeader("User-Agent");
+		String url = request.getRequestURI();
+		String requestParams = WebUtilsEx.getParametersJson(request);
+		FrameLogOperate entity=new FrameLogOperate();
+		entity.setSystemHost(BaseUtils.getServerIp()+ ":" + request.getLocalPort());
+		String userHost=BaseUtils.getIpAddr(request);
+		entity.setParams(requestParams);
+		entity.setType(optLog.type());
+		entity.setContent(optLog.detail());
+		entity.setUserName(SecurityUtilsHelper.getCurrentUser().getUserName());
+		entity.setUserId(SecurityUtilsHelper.getCurrentUser().getUserId());
+		entity.setUserHost(userHost);
+		entity.setUserAgent(userAgent);
+		logLoginService.optlog(entity);
+	}
+	
+}
+
