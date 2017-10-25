597e65964b14c623798fb6bc5528788fe2fa8bde
gg <lkg61230413@163.com>
2017-10-25 14:38:36 +0800

    移除无效代码

--- heartwork/work-conf/src/main/java/im/heart/App.java
+++ heartwork/work-conf/src/main/java/im/heart/App.java
@@ -10,7 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
+/*import org.springframework.context.annotation.PropertySource;*/
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @Configuration
