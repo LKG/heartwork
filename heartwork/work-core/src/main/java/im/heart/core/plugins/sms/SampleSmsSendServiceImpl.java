18e7676cf9e680b446e70abf10814b40567b122d
gg <lkg61230413@163.com>
2017-10-25 14:26:31 +0800

    移除无效代码

--- heartwork/work-core/src/main/java/im/heart/core/plugins/sms/SampleSmsSendServiceImpl.java
+++ heartwork/work-core/src/main/java/im/heart/core/plugins/sms/SampleSmsSendServiceImpl.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
